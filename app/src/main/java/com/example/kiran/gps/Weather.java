d7f45351ce97ae40414e3f16bb49a28c60364539
kiran <kirankumarh66@gmail.com>
2018-08-24 15:50:24 +0530

    Created new class called Weather which holds the logic for finding weather status.

--- app/src/main/java/com/example/kiran/gps/Weather.java
+++ app/src/main/java/com/example/kiran/gps/Weather.java
@@ -9,7 +9,6 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
-
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,17 +16,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Weather {
     private final MainActivity mainActivity;
-
     public Weather(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
     }
-
     void findWeather() {
         LocationManager locationManager;
         locationManager = (LocationManager) mainActivity.getSystemService(Context.LOCATION_SERVICE);
