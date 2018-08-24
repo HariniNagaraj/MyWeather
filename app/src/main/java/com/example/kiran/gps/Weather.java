8bc24a636efc93e31f4d69a0877a9b01cb75096b
kiran <kirankumarh66@gmail.com>
2018-08-24 23:36:55 +0530

    Merge branch 'master' of https://github.com/Lunarkiran/MyWeather
    # Conflicts:
    #	app/src/main/java/com/example/kiran/gps/MainActivity.java

--- app/src/main/java/com/example/kiran/gps/Weather.java
+++ app/src/main/java/com/example/kiran/gps/Weather.java
@@ -21,13 +21,13 @@ import org.json.JSONObject;
 
 public class Weather {
     private final MainActivity mainActivity;
+    private double latitude = 0.0, longitude = 0.0;
     public Weather(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
     }
     void findWeather() {
         LocationManager locationManager;
         locationManager = (LocationManager) mainActivity.getSystemService(Context.LOCATION_SERVICE);
-        double latitude = 0.0, longitude = 0.0;
         if (ActivityCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED & ActivityCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -36,39 +36,45 @@ public class Weather {
                 latitude = location.getLatitude();
                 longitude = location.getLongitude();
             }
-            String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b3e236d068148443f565e441eacf0a84&units=metric";
-            JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                @RequiresApi(api = Build.VERSION_CODES.O)
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        JSONObject main_object = response.getJSONObject("main");
-                        JSONObject main_object2 = response.getJSONObject("wind");
-                        String temperature = String.valueOf(main_object.getInt("temp"));
-                        String city = response.getString("name");
-                        String wind = String.valueOf(main_object2.getInt("speed") * 3.6 + " kph");
-                        String humidity = String.valueOf(main_object.getInt("humidity") + "%");
-                        mainActivity.getTemp().setText(temperature);
-                        mainActivity.getPlace().setText(city);
-                        mainActivity.getHumid().setText(humidity);
-                        mainActivity.getAir().setText(wind);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    NetworkResponse networkResponse = error.networkResponse;
-                    if (networkResponse != null && networkResponse.data != null) {
-                        String jsonError = new String(networkResponse.data);
-                        Toast.makeText(mainActivity, jsonError,
-                                Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-            RequestQueue queue = Volley.newRequestQueue(mainActivity);
-            queue.add(jor);
+            getJsonData();
+
         }
+    }
+
+    private void getJsonData() {
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b3e236d068148443f565e441eacf0a84&units=metric";
+        JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject main_object = response.getJSONObject("main");
+                    JSONObject main_object2 = response.getJSONObject("wind");
+                    String temperature = String.valueOf(main_object.getInt("temp"));
+                    String city = response.getString("name");
+                    String wind = String.valueOf(main_object2.getInt("speed") * 3.6 + " kph");
+                    String humidity = String.valueOf(main_object.getInt("humidity") + "%");
+                    mainActivity.getTemp().setText(temperature);
+                    mainActivity.getPlace().setText(city);
+                    mainActivity.getHumid().setText(humidity);
+                    mainActivity.getAir().setText(wind);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null && networkResponse.data != null) {
+                    String jsonError = new String(networkResponse.data);
+                    Toast.makeText(mainActivity, jsonError,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        RequestQueue queue = Volley.newRequestQueue(mainActivity);
+        queue.add(jor);
+
     }
 }
