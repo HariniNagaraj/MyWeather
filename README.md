eb93157a5cb73d5603803345f126be867cf9a5e8
kiran <kirankumarh66@gmail.com>
2018-08-24 16:37:50 +0530

    Created new class called Weather which holds the logic for finding weather status.

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # MyWeather
-![](images/Screenshot.png)
-
-Steps to Build this app.
-Get your API key by signing in.              https://home.openweathermap.org/api_keys
-data can be fetched using JSON,XML or HTML in this app i used JSON. 
-You can get current weather data by using cityname,cityid,zipcode,coordinates and i used coordinates from gps. for more info https://openweathermap.org/api
-This app uses permission  android.permission.INTERNET,android.permission.ACCESS_COARSE_LOCATION,android.permission.ACCESS_FINE_LOCATION" />
-    and i used volley library for http request.
-    Dependencies used ---->  compile 'com.android.volley:volley:1.0.0'
-   feel free to make any changes.
-    
--- app/src/main/java/com/example/kiran/gps/MainActivity.java
+++ app/src/main/java/com/example/kiran/gps/MainActivity.java
@@ -5,115 +5,222 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.widget.TextView;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
-    private final Weather weather = new Weather(this);
-    private SwipeRefreshLayout pullToRefresh;
-    protected TextView place, humid, air, date,temp;
-    private Intent intent;
-    private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 1;
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+
+    LocationManager locationManager;
+    TextView place, humid, air;
+    TextView date;
+    protected static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 1;
+    TextView temp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        screenRefresher();
-        setUIElements();
-        showLocationSettingsDialog();
-        requestReadLocationPermission();
-        setCurrentDate();
-        weather.findWeather();
-    }
-
-    private void screenRefresher() {
-        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                weather.findWeather();
-                pullToRefresh.setRefreshing(false);
-            }
-        });
-    }
-
-    private void setUIElements() {
-        pullToRefresh = findViewById(R.id.pullToRefresh);
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
         date = findViewById(R.id.day);
+        temp = findViewById(R.id.celcius);
         place = findViewById(R.id.city);
         humid = findViewById(R.id.humidity);
         air = findViewById(R.id.wind);
-        temp = findViewById(R.id.celcius);
+        requestReadLocationPermission();
+        showLocationSettingsDialog();
+        find_weather();
+
+
     }
 
-    private void setCurrentDate() {
-        Date c = Calendar.getInstance().getTime();
-        SimpleDateFormat df = new SimpleDateFormat("EEEE, MMM dd", Locale.getDefault());
-        String formattedDate = df.format(c);
-        date.setText(formattedDate);
-    }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void requestReadLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MainActivity.MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+    private void find_weather() {
+
+        double latti = 0, longi = 0;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED & ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+
+            if (location != null) {
+                latti = location.getLatitude();
+                longi = location.getLongitude();
             }
-    }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void showLocationSettingsDialog() {
-        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (!Objects.requireNonNull(manager).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-            alertDialog.setTitle("Location Settings");
-            alertDialog.setMessage("Enable GPS press cancel if already set");
-            alertDialog.setPositiveButton("SETTINGS", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(intent);
+            String urlparse = "https://api.openweathermap.org/data/2.5/weather?lat=" + latti + "&lon=" + longi + "&appid=b3e236d068148443f565e441eacf0a84&units=metric";
+            String url = urlparse;
+            JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONObject main_object = response.getJSONObject("main");
+                        JSONObject main_object2 = response.getJSONObject("wind");
+                        String temperature = String.valueOf(main_object.getInt("temp"));
+                        String city = response.getString("name");
+                        String wind = String.valueOf(main_object2.getInt("speed") * 3.6 + " kph");
+                        String humidity = String.valueOf(main_object.getInt("humidity") + "%");
+
+
+                        Calendar calendar = Calendar.getInstance();
+                        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, MMM dd");
+                        String formatted_date = sdf.format(calendar.getTime());
+
+                        date.setText(formatted_date);
+
+
+                        temp.setText(temperature);
+                        place.setText(city);
+                        humid.setText(humidity);
+                        air.setText(wind);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
                 }
-            });
-            alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
                 }
-            });
-            alertDialog.show();
+            }
+            );
+            RequestQueue queue = Volley.newRequestQueue(this);
+            queue.add(jor);
+
         }
     }
 
-    public TextView getTemp() {
-        return temp;
+    private void requestReadLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Explanation not needed, since user requests this themself
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MainActivity.MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+            }
+        } else {
+            privacyGuardWorkaround();
+        }
+        togglegps();
+
     }
 
-    public TextView getPlace() {
-        return place;
+    private void togglegps() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showLocationSettingsDialog();
+        } else {
+            return;
+        }
     }
 
-    public TextView getHumid() {
-        return humid;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                }
+                return;
+            }
+        }
     }
 
-    public TextView getAir() {
-        return air;
+    private void privacyGuardWorkaround() {
+        // Workaround for CM privacy guard. Register for location updates in order for it to ask us for permission
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            DummyLocationListener dummyLocationListener = new DummyLocationListener();
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, dummyLocationListener);
+            locationManager.removeUpdates(dummyLocationListener);
+        } catch (SecurityException e) {
+            // This will most probably not happen, as we just got granted the permission
+        }
     }
+
+    private void showLocationSettingsDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Location Settings");
+        alertDialog.setMessage("Enable GPS press cancel if already set");
+        alertDialog.setPositiveButton("SETTINGS", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+        alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.show();
+
+    }
+
+    public class DummyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
 }
 
 
