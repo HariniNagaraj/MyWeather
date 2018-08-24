687d8cfde3be08a095ded31c1d346139b33c50d4
kiran <kirankumarh66@gmail.com>
2018-08-24 15:47:52 +0530

    Created new class called Weather which holds the logic for finding weather status.

--- app/src/main/java/com/example/kiran/gps/MainActivity.java
+++ app/src/main/java/com/example/kiran/gps/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
@@ -17,16 +16,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -34,8 +23,9 @@ import java.util.Locale;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private final Weather weather = new Weather(this);
     private SwipeRefreshLayout pullToRefresh;
-    private TextView place, humid, air, date,temp;
+    protected TextView place, humid, air, date,temp;
     private Intent intent;
     private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 1;
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -49,14 +39,14 @@ public class MainActivity extends AppCompatActivity {
         showLocationSettingsDialog();
         requestReadLocationPermission();
         setCurrentDate();
-        findWeather();
+        weather.findWeather();
     }
 
     private void screenRefresher() {
         pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                findWeather();
+                weather.findWeather();
                 pullToRefresh.setRefreshing(false);
             }
         });
@@ -75,54 +65,6 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat df = new SimpleDateFormat("EEEE, MMM dd", Locale.getDefault());
         String formattedDate = df.format(c);
         date.setText(formattedDate);
-    }
-
-    private void findWeather() {
-        LocationManager locationManager;
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        double latitude = 0.0, longitude = 0.0;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED & ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            if (location != null) {
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
-            }
-            String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b3e236d068148443f565e441eacf0a84&units=metric";
-            JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                @RequiresApi(api = Build.VERSION_CODES.O)
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        JSONObject main_object = response.getJSONObject("main");
-                        JSONObject main_object2 = response.getJSONObject("wind");
-                        String temperature = String.valueOf(main_object.getInt("temp"));
-                        String city = response.getString("name");
-                        String wind = String.valueOf(main_object2.getInt("speed") * 3.6 + " kph");
-                        String humidity = String.valueOf(main_object.getInt("humidity") + "%");
-                        temp.setText(temperature);
-                        place.setText(city);
-                        humid.setText(humidity);
-                        air.setText(wind);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    NetworkResponse networkResponse = error.networkResponse;
-                    if (networkResponse != null && networkResponse.data != null) {
-                        String jsonError = new String(networkResponse.data);
-                        Toast.makeText(MainActivity.this, jsonError,
-                                Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-            RequestQueue queue = Volley.newRequestQueue(this);
-            queue.add(jor);
-        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -153,6 +95,22 @@ public class MainActivity extends AppCompatActivity {
             });
             alertDialog.show();
         }
+    }
+
+    public TextView getTemp() {
+        return temp;
+    }
+
+    public TextView getPlace() {
+        return place;
+    }
+
+    public TextView getHumid() {
+        return humid;
+    }
+
+    public TextView getAir() {
+        return air;
     }
 }
 
