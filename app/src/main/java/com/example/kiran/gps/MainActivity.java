60ac796a3a7d4c064e7f2aec96fad6e97ee047c5
kiran <kirankumarh66@gmail.com>
2018-08-27 09:53:54 +0530

    Fixed some bugs on onRequestPermissionsResult and little refactoring.

--- app/src/main/java/com/example/kiran/gps/MainActivity.java
+++ app/src/main/java/com/example/kiran/gps/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
@@ -31,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected static TextView  place,humid, air, date,temp;
     private double latitude, longitude;
     private Intent intent;
-    private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 1;
+    private static final int requestCode = 1;
     private static String[] PERMISSIONS_GPS = {Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION};
 
@@ -44,11 +43,11 @@ public class MainActivity extends AppCompatActivity {
         bindUIElements();
         requestReadLocationPermission();
         showCurrentDate();
-        locationServices();
+        manageLocationServices();
         initScreenRefresh();
     }
 
-    private void locationServices() {
+    private void manageLocationServices() {
         LocationManager locationManager;
         locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -59,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
                 latitude = location.getLatitude();
                 longitude = location.getLongitude();
             }
-
         }
     }
 
@@ -68,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                locationServices();
+                manageLocationServices();
                 weather.findWeather(latitude,longitude);
                 pullToRefresh.setRefreshing(false);
             }
@@ -93,27 +91,24 @@ public class MainActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void requestReadLocationPermission() {
-
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MainActivity.MY_PERMISSIONS_ACCESS_FINE_LOCATION);
-
+                    MainActivity.requestCode);
             }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onRequestPermissionsResult(int MY_PERMISSIONS_ACCESS_FINE_LOCATION, String[] PERMISSIONS_GPS, int[] grantResults) {
-
         switch (MY_PERMISSIONS_ACCESS_FINE_LOCATION) {
             case 1: {
                 if ((grantResults.length == 1 && grantResults[0]  == PackageManager.PERMISSION_GRANTED) ){
                     //If user presses allow
                     Toast.makeText(MainActivity.this, "Permission granted!", Toast.LENGTH_SHORT).show();
-                   showLocationSettingsDialog();}
-                   else{
+                   showLocationSettingsDialog();
+                }
+                else{
                     Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
                 }
                 break;
