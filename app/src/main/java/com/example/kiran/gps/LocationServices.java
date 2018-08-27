74ea1c661f3dff61f57369cb50f8080ebdef1749
kiran <kirankumarh66@gmail.com>
2018-08-27 16:23:54 +0530

    locationservice class added

--- app/src/main/java/com/example/kiran/gps/LocationServices.java
+++ app/src/main/java/com/example/kiran/gps/LocationServices.java
@@ -2,30 +2,97 @@ package com.example.kiran.gps;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class LocationServices {
+import java.util.Objects;
+
+public class LocationServices extends AppCompatActivity {
     protected double latitude, longitude;
-    private final Context context;
-
-    public LocationServices(Context context) {
-        this.context = context;
+    private final MainActivity mainActivity;
+    public LocationServices(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
     }
 
-    void manageLocationServices() {
+    void fetchLocation() {
         LocationManager locationManager;
-        locationManager = (LocationManager) context.getSystemService(MainActivity.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED & ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+        locationManager = (LocationManager) mainActivity.getSystemService(MainActivity.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED & ActivityCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             if (location != null) {
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
+               latitude=location.getLatitude();
+                longitude=location.getLongitude();
             }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    void isGPSEnabled() {
+        if (ActivityCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED & ActivityCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            showLocationSettingsDialog();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    void requestReadLocationPermission() {
+        if (ContextCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(mainActivity,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MainActivity.CONTEXT_INCLUDE_CODE);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onRequestPermissionsResult(int MY_PERMISSIONS_ACCESS_FINE_LOCATION, String[] PERMISSIONS_GPS, int[] grantResults) {
+        switch (MY_PERMISSIONS_ACCESS_FINE_LOCATION) {
+            case 1: {
+                if ((grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //If user presses allow
+                    Toast.makeText(mainActivity, "Permission granted!", Toast.LENGTH_SHORT).show();
+                    showLocationSettingsDialog();
+                } else {
+                    Toast.makeText(mainActivity, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    void showLocationSettingsDialog() {
+        final LocationManager manager = (LocationManager) mainActivity.getSystemService(Context.LOCATION_SERVICE);
+        if (!Objects.requireNonNull(manager).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mainActivity);
+            alertDialog.setTitle("Location Settings");
+            alertDialog.setMessage("Permission Granted, Please Enable GPS in Settings.");
+            alertDialog.setPositiveButton("SETTINGS", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    mainActivity.setIntent(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    mainActivity.startActivity(mainActivity.getIntent());
+                }
+            });
+            alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            alertDialog.show();
         }
     }
 }
--- app/src/main/java/com/example/kiran/gps/MainActivity.java
+++ app/src/main/java/com/example/kiran/gps/MainActivity.java
@@ -1,28 +1,15 @@
 package com.example.kiran.gps;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
 import android.os.Build;
-import android.provider.Settings;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Objects;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -37,10 +24,6 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.wind) TextView air;
     @BindView(R.id.humidity) TextView humid;
 
-
-    private Intent intent;
-    private static final int requestCode = 1;
-
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,25 +31,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Pull To Refresh");
         ButterKnife.bind(this);
-        requestReadLocationPermission();
+        locationServices.requestReadLocationPermission();
         showCurrentDate();
-        locationServices.manageLocationServices();
+        locationServices.fetchLocation();
         initScreenRefresh();
-    }
-
-    private void manageLocationServices() {
-        locationServices.manageLocationServices();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void initScreenRefresh() {
-        isGPSEnabled();
+        locationServices.isGPSEnabled();
         updateWeather();
         pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                isGPSEnabled();
-                locationServices.manageLocationServices();
+                locationServices.isGPSEnabled();
+                locationServices.fetchLocation();
                 updateWeather();
                 pullToRefresh.setRefreshing(false);
             }
@@ -85,68 +64,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    void isGPSEnabled() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED & ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            showLocationSettingsDialog();
-        }
-    }
-
     private void showCurrentDate() {
         Date day = Calendar.getInstance().getTime();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMM dd", Locale.getDefault());
         String formattedDate = simpleDateFormat.format(day);
         date.setText(formattedDate);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void requestReadLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MainActivity.requestCode);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Override
-    public void onRequestPermissionsResult(int MY_PERMISSIONS_ACCESS_FINE_LOCATION, String[] PERMISSIONS_GPS, int[] grantResults) {
-        switch (MY_PERMISSIONS_ACCESS_FINE_LOCATION) {
-            case 1: {
-                if ((grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    //If user presses allow
-                    Toast.makeText(MainActivity.this, "Permission granted!", Toast.LENGTH_SHORT).show();
-                    showLocationSettingsDialog();
-                } else {
-                    Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            }
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void showLocationSettingsDialog() {
-        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (!Objects.requireNonNull(manager).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-            alertDialog.setTitle("Location Settings");
-            alertDialog.setMessage("Permission Granted, Please Enable GPS in Settings.");
-            alertDialog.setPositiveButton("SETTINGS", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    startActivity(intent);
-                }
-            });
-            alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
-                }
-            });
-            alertDialog.show();
-        }
     }
 }
 
